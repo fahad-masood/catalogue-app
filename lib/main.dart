5bac02c13c904ede15d2df45e2b579fd1137bb90
Fahad <fahadmasood03@gmail.com>
2021-04-27 03:51:16 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalogue/home_page.dart';
+import 'package:flutter_catalogue/pages/home_page.dart';
+import 'package:flutter_catalogue/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
-    );
+        //home: HomePage(),
+        themeMode: ThemeMode.light,
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+        ),
+        initialRoute: '/home', //Sets the first screen which is initially '/'
+        routes: {
+          "/": (context) => LoginPage(),
+          "/home": (context) => HomePage(),
+          "/login": (context) => LoginPage(),
+        });
   }
 }
