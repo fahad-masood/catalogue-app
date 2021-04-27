46b6419ab7a281a01f68dce99db9f87441cea726
Fahad <fahadmasood03@gmail.com>
2021-04-28 05:06:39 +0530

    Day 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalogue/pages/home_page.dart';
 import 'package:flutter_catalogue/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,15 +11,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        //color: Colors.white,
         //home: HomePage(),
         themeMode: ThemeMode.light,
         theme: ThemeData(
+          fontFamily: GoogleFonts.lato().fontFamily,
           primarySwatch: Colors.green,
         ),
         darkTheme: ThemeData(
           brightness: Brightness.dark,
         ),
-        initialRoute: '/home', //Sets the first screen which is initially '/'
+        initialRoute: '/', //Sets the first screen which is initially '/'
         routes: {
           "/": (context) => LoginPage(),
           "/home": (context) => HomePage(),
